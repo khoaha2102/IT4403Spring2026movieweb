0e934cb0e4e8683db69fa8712e0788096492f0ea
khoaha2102 <kha2@students.kennesaw.edu>
2026-04-20 23:20:40 -0400

    api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,23 @@
+const API_KEY = "8d6d0afa1e5173177b90d97661c359fe";
+const BASE_URL = "https://api.themoviedb.org/3";
 
+function getPopularMovies() {
+  return $.ajax({
+    url: `${BASE_URL}/movie/popular`,
+    method: "GET",
+    data: {
+      api_key: API_KEY
+    }
+  });
+}
+
+function searchMovies(query) {
+  return $.ajax({
+    url: `${BASE_URL}/search/movie`,
+    method: "GET",
+    data: {
+      api_key: API_KEY,
+      query: query
+    }
+  });
+}
